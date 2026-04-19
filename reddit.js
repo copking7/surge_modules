cc865e7c59b4e19691384e2896ad2e96716481a1
Olivia Carter <275704587+copking7@users.noreply.github.com>
2026-04-19 23:34:55 +0800

    Update Reddit translation script parameters

--- reddit.js
+++ reddit.js
@@ -1,12 +1,12 @@
 #!name=Reddit 网页翻译参数
-#!desc=为 Reddit 网页请求自动翻译
+#!desc=仅为 Reddit页面路由翻译
 #!category=reddit
 
 [General]
-force-http-engine-hosts = %APPEND% reddit.com, www.reddit.com
+force-http-engine-hosts = %APPEND% www.reddit.com
 
 [MITM]
-hostname = %APPEND%, reddit.com, www.reddit.com
+hostname = %APPEND%, www.reddit.com
 
 [Script]
-reddit-tl = type=http-request,pattern=^https:\/\/(www\.)?reddit\.com\/,requires-body=0,script-path=https://raw.githubusercontent.com/copking7/surge_modules/refs/heads/main/reddit_tl.js
+reddit-tl = type=http-request,pattern=^https:\/\/www\.reddit\.com\/r\/,requires-body=0,script-path=https://raw.githubusercontent.com/copking7/surge_modules/main/reddit_tl.js

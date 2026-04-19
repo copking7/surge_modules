97957df54cd44947f7d32a9ab27bd673a4496fb6
Olivia Carter <275704587+copking7@users.noreply.github.com>
2026-04-19 23:29:07 +0800

    Update reddit.js

--- reddit.js
+++ reddit.js
@@ -1,16 +1,12 @@
 #!name=Reddit 网页翻译参数
-#!desc=为 Reddit 网页请求翻译
+#!desc=为 Reddit 网页请求自动翻译
 #!category=reddit
 
 [General]
-force-http-engine-hosts = %APPEND% www.reddit.com
+force-http-engine-hosts = %APPEND% reddit.com, www.reddit.com
 
 [MITM]
-hostname = %APPEND%, www.reddit.com
+hostname = %APPEND%, reddit.com, www.reddit.com
 
-[URL Rewrite]
-# 没有 query 参数时，追加 ?tl=zh-hans
-^https:\/\/www\.reddit\.com\/([^?]+)$ https://www.reddit.com/$1?tl=zh-hans 302
-
-# 已有 query 参数但没有 tl= 时，追加 &tl=zh-hans
-^https:\/\/www\.reddit\.com\/([^?]+)\?((?!.*(?:^|&)tl=).*)$ https://www.reddit.com/$1?$2&tl=zh-hans 302
+[Script]
+reddit-tl = type=http-request,pattern=^https:\/\/(www\.)?reddit\.com\/,requires-body=0,script-path=https://raw.githubusercontent.com/copking7/surge_modules/refs/heads/main/reddit_tl.js

79ec7d5ebfed28b6c8148eee1b33d98499a62800
Olivia Carter <275704587+copking7@users.noreply.github.com>
2026-04-19 23:21:59 +0800

    Modify Reddit translation parameters and rewrites
    Updated translation parameters for Reddit web requests.

--- reddit.js
+++ reddit.js
@@ -1,7 +1,16 @@
 #!name=Reddit 网页翻译参数
-#!desc=访问 Reddit 网页时自动追加 tl=zh-hans
+#!desc=为 Reddit 网页请求翻译
 #!category=reddit
 
+[General]
+force-http-engine-hosts = %APPEND% www.reddit.com
+
+[MITM]
+hostname = %APPEND%, www.reddit.com
+
 [URL Rewrite]
+# 没有 query 参数时，追加 ?tl=zh-hans
 ^https:\/\/www\.reddit\.com\/([^?]+)$ https://www.reddit.com/$1?tl=zh-hans 302
+
+# 已有 query 参数但没有 tl= 时，追加 &tl=zh-hans
 ^https:\/\/www\.reddit\.com\/([^?]+)\?((?!.*(?:^|&)tl=).*)$ https://www.reddit.com/$1?$2&tl=zh-hans 302
